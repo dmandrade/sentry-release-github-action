ff8f00a4045d7338747d05d68674ffa160bfe9c5
Danilo Andrade <mandrade.danilo@gmail.com>
2019-10-24 19:25:12 -0300

    fix: add new input data to set auth token, sentry url and org/project

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
-const exec = require('./src/_exec');
-const path = require('path');
+const core = require('@actions/core');
+const SentryCli = require('@sentry/cli');
+const sentry = new SentryCli();
 
-const run = async () => {
-    // Install Dependencies
-    {
-        const {stdout, stderr} = await exec('npm ci --only=prod', {
-            cwd: path.resolve(__dirname)
-        });
-        console.log(stdout);
-        if (stderr) {
-            return Promise.reject(stderr);
+// most @actions toolkit packages have async methods
+async function run() {
+    try {
+        const authToken = core.getInput('auth-token', {required: true});
+        const sentryUrl = core.getInput('url', {required: true}) || 'https://sentry.io/';
+        const sentryOrg = core.getInput('org', {required: true});
+        const sentryProject = core.getInput('project', {required: true});
+        const releaseVersion = core.getInput('version', {required: true});
+        const deployEnvironment = core.getInput('environment', {required: false}) || '';
+
+        const exec = async (arguments) => {
+            await sentry.execute([
+                "--auth-token",
+                authToken,
+                "--url",
+                sentryUrl,
+                "--org",
+                sentryOrg,
+                "--project",
+                sentryProject
+            ].concat(arguments));
+        }
+
+        await exec([
+            "releases",
+            "new",
+            releaseVersion
+        ]);
+        core.debug(`The release "${releaseVersion}" was sended to Sentry.`);
+
+        await exec([
+            "releases",
+            "set-commits",
+            "--auto",
+            releaseVersion
+        ]);
+        core.debug(`Commits are associated with release "${releaseVersion}".`);
+
+        if(deployEnvironment) {
+            await exec([
+                "releases",
+                "deploys",
+                releaseVersion,
+                'new',
+                '-e',
+                deployEnvironment
+            ]);
+            core.debug(`The release "${releaseVersion}" are associated with "${deployEnvironment}" environment.`);
         }
     }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
 
-    require('./src/index')();
-};
-
-run().catch(console.error);
+run()
--- src/_exec.js
+++
@@ -1,2 +0,0 @@
-const util = require('util');
-module.exports = util.promisify(require('child_process').exec);
