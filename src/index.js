ab9c16958fa033f7614dfeb5a219dc7bfc40d8f4
Danilo Andrade <mandrade.danilo@gmail.com>
2019-10-24 19:02:28 -0300

    try to require sentry cli

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-const exec = require('@actions/exec');
 const core = require('@actions/core');
-
+const SentryCli = require('@sentry/cli');
+const sentry = new SentryCli();
 /**
  * Release task
  * @returns {Promise<never>}
  */
 const release = async () => {
-    const baseCommand = 'npx sentry-cli releases';
-    const commandOptions = {
-        failOnStdErr: true,
-    };
     const releaseVersion = core.getInput('version', {required: true});
     const deployEnvironment = core.getInput('environment', {required: false}) || '';
 
-    await exec.exec(baseCommand, [
+    await sentry.execute([
+        "releases",
         "new",
         releaseVersion
-    ], commandOptions);
+    ]);
     core.debug(`The release "${releaseVersion}" was sended to Sentry.`);
 
-    await exec.exec(baseCommand, [
+    await sentry.execute([
+        "releases",
         "set-commits",
         "--auto",
         releaseVersion
-    ], commandOptions);
+    ]);
     core.debug(`Commits are associated with release "${releaseVersion}".`);
 
     if(deployEnvironment) {
-        await exec.exec(baseCommand, [
+        await sentry.execute([
+            "releases",
             "deploys",
             releaseVersion,
             'new',
             '-e',
             deployEnvironment
-        ], commandOptions);
+        ]);
         core.debug(`The release "${releaseVersion}" are associated with "${deployEnvironment}" environment.`);
     }
 };
