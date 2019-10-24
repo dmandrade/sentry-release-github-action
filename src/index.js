6f1598074c4bd12e046e658f3d9b9a67d6037100
Danilo Andrade <mandrade.danilo@gmail.com>
2019-10-24 19:57:21 -0300

    fix: remove arguments and use env variables

--- src/index.js
+++ src/index.js
@@ -6,33 +6,18 @@ const SentryCli = require('@sentry/cli');
  * @returns {Promise<never>}
  */
 const release = async () => {
-    const authToken = core.getInput('auth-token', {required: true});
-    const sentryUrl = core.getInput('url', {required: true}) || 'https://sentry.io/';
-    const sentryOrg = core.getInput('org', {required: true});
-    const sentryProject = core.getInput('project', {required: true});
     const releaseVersion = core.getInput('version', {required: true});
     const deployEnvironment = core.getInput('environment', {required: false}) || '';
+    const sentry = new SentryCli();
 
-
-    const sentry = new SentryCli(null, {
-        auth_token: authToken,
-        url: sentryUrl,
-        org: sentryOrg,
-        project: sentryProject
-    });
-
-    const exec = async (arguments) => {
-        await sentry.execute(arguments);
-    }
-
-    await exec([
+    await sentry.execute([
         "releases",
         "new",
         releaseVersion
     ]);
     core.debug(`The release "${releaseVersion}" was sended to Sentry.`);
 
-    await exec([
+    await sentry.execute([
         "releases",
         "set-commits",
         "--auto",
@@ -41,7 +26,7 @@ const release = async () => {
     core.debug(`Commits are associated with release "${releaseVersion}".`);
 
     if(deployEnvironment) {
-        await exec([
+        await sentry.execute([
             "releases",
             "deploys",
             releaseVersion,
